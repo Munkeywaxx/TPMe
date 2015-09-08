de42d5234849bf4c69c9fd3181842b9e1e756b11
Munkeywaxx <Munkeywaxx@gmail.com>
2015-09-08 01:08:15 +0100

    Added lots of stuff in the last hour

--- src/main/java/Munkeywaxx/TPMe/Main.java
+++ src/main/java/Munkeywaxx/TPMe/Main.java
@@ -1,8 +1,49 @@
 package Munkeywaxx.TPMe;
 
+import java.util.Random;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
+	
+	@Override
+	public void onEnable(){
+		getLogger().info("TPMe enabled.");
+		new TPMeListener(this);
+		
+	}
+		
+		@Override
+		public void onDisable(){
+			getLogger().info("Disabled.");
+		
+	}
+		
+		public boolean onCommand (CommandSender sender, Command cmd, String label, String[] args) {
+			
+			if (cmd.getName().equalsIgnoreCase("TPMe") && sender instanceof Player) {
+				Player player = (Player) sender;
+				Location originalLocation = player.getLocation();
+				
+				Random random = new Random();
+				int x = random.nextInt(5000) +1;
+				int y = 150;
+				int z = random.nextInt(5000) +1;
+				
+				Location teleportLocation = new Location(player.getWorld(), x, y, z);
+				player.teleport(teleportLocation);
+				
+				player.sendMessage(ChatColor.GREEN + "Teleported " + (int)teleportLocation.distance(originalLocation) + " Blocks Away.");
+				
+				return true;
+			}
+			return false;
+		}
 	
 	
 }
---
+++ src/main/java/Munkeywaxx/TPMe/TPMeListener.java
@@ -0,0 +1,35 @@
+package Munkeywaxx.TPMe;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class TPMeListener implements Listener {
+	
+	public TPMeListener(Main main) {
+		
+		main.getServer().getPluginManager().registerEvents(this, main);
+	}
+	
+
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent s) {
+		Player player = s.getPlayer();
+		s.setJoinMessage(ChatColor.AQUA + "Welcome back " + player.getName());
+		
+		if (player.hasPlayedBefore() == true) {
+			player.sendMessage(ChatColor.BLUE + "Welcome back!");
+			
+		} else if (player.hasPlayedBefore() == false) {
+			s.setJoinMessage(player.getName() + ChatColor.GOLD + "Is a new guy. Welcome!");
+			
+			
+		} 
+			
+			
+		}
+	}
+
+
